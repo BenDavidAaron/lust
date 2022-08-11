c304f89d030fa59cff84e95254bff2acdc54a739
Ben Aaron <ben@theaarons.com>
2022-08-10 23:04:49 -0700

    almost implement tokenize

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+fn tokenize(chars: &str) -> Vec<str>{
+    let left_padded = str::replace(chars, "(", " ( ");
+    let right_padded = str::replace(&left_padded, ")", " ) ");
+    let tokens: Vec<&str> = right_padded.split_whitespace().collect();
+    return tokens
 }
+
+fn main() {
+    let program = "(* 3 (+ 2 2))";
+    println!("{}", program);
+    let tokens = tokenize(program);
+    println!("{}", tokens);
+}
+
