6264d6517b18dde0fec3933cf1d9696ee8a04d5d
Ben Aaron <ben@theaarons.com>
2022-09-17 12:28:37 -0700

    start parser

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ struct LustEnv {
     data: HashMap<String, LustExpression>,
 }
 
-pub fn tokenize(expression: String) -> Vec<String> {
+fn tokenize(expression: String) -> Vec<String> {
     expression
         .replace("(", " ( ")
         .replace(")", " ) ")
@@ -25,12 +25,43 @@ pub fn tokenize(expression: String) -> Vec<String> {
         .collect()
 }
 
+fn parse<'a>(tokens: &'a [String]) -> Result<(LustExpression, &'a [String]), LustException> {
+    let (token, rest) = tokens.split_first().ok_or(LustException::Reason(
+        "Bruh, I can't get the next Token".to_string(),
+    ))?;
+    match &token[..] {
+        "(" => read_seq(rest),
+        ")" => Err(LustException::Reason(
+            "Bruh you put `)` in the wrong place!".to_string(),
+        )),
+    }
+}
+
+fn read_seq<'a>(tokens: &'a [String]) -> Result<(LustExpression, &'a [String]), LustException> {
+    let mut res: Vec<LustExpression> = vec![];
+    let mut xs = tokens;
+    loop {
+        let (next_token, rest) = xs.split_first().ok_or(LustException::Reason(
+            "Bruh you forgot a `)` somewhere!".to_string(),
+        ))?;
+        if next_token == ")" {
+            return Ok((LustExpression::List(res), rest)); // skip closing parens, go to next
+        }
+        let (exp, new_xs) = parse(&xs)?;
+        res.push(exp);
+        xs = new_xs;
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
     const SIMPLE_PROGRAM: &str = "(+ 2 2)";
     #[test]
     fn tokenize_simple_program() {
-        assert_eq!(tokenize(SIMPLE_PROGRAM.to_string()), vec!["(", "+", "2", "2", ")"])
+        assert_eq!(
+            tokenize(SIMPLE_PROGRAM.to_string()),
+            vec!["(", "+", "2", "2", ")"]
+        )
     }
 }
